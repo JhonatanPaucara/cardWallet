f6bac892c07d72e8a608370ba816629f036e0539
JhonatanPaucara <jhonatan.paucara@gmail.com>
2019-08-08 17:02:41 -0500

    First connection to Firestore

---
+++ lib/ui/myhomepage.dart
@@ -0,0 +1,61 @@
+import 'package:flutter/material.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+
+class MyHomePage extends StatelessWidget {
+  MyHomePage({Key key, this.title}) : super(key: key);
+  final String title;
+  Widget _buildListCard(BuildContext context, DocumentSnapshot document) {
+    return ListTile(
+      title: Row(
+        children: <Widget>[
+          Expanded(
+            child: Text(
+              document['name'],
+              style: Theme.of(context).textTheme.headline,
+            ),
+          ),
+          Container(
+            decoration: BoxDecoration(
+              color: Colors.red,
+            ),
+            padding: EdgeInsets.all(10.0),
+            child: Text(
+              document['cvv'].toString(),
+              style: Theme.of(context).textTheme.display1,
+            ),
+          ),
+        ],
+      ),
+      onTap: () {
+        Firestore.instance.runTransaction((transaction) async {
+          DocumentSnapshot newSnap = await transaction.get(document.reference);
+          await transaction.update(
+            newSnap.reference,
+            {'cvv': document['cvv'] + 1},
+          );
+        });
+      },
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(title),
+      ),
+      body: StreamBuilder(
+        stream: Firestore.instance.collection('Cards').snapshots(),
+        builder: (context, snapshot) {
+          if (!snapshot.hasData) return Text('...Loading');
+          return ListView.builder(
+            itemExtent: 80.0,
+            itemCount: snapshot.data.documents.length,
+            itemBuilder: (context, index) =>
+                _buildListCard(context, snapshot.data.documents[index]),
+          );
+        },
+      ),
+    );
+  }
+}
